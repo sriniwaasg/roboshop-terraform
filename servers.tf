ad49aa3aac066cc3eb3114a73fc371732f1dab8e
HP <sriniwassg23@gmail.com>
2023-09-03 21:27:09 +0530

    catalogue script

--- servers.tf
+++ servers.tf
@@ -1,12 +1,11 @@
 data "aws_ami" "centos" {
   owners      = ["973714476881"]
   most_recent = true
-  name_regex  = "Centos-8-DevOps-practice"
+  name_regex  = "Centos-8-DevOps-Practice"
 }
 output "ami" {
  value = data.aws_ami.centos.image_id
 }
-
 
 resource "aws_instance" "frontend" {
   ami           = "ami-03265a0778a880afb"
